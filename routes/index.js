8bd8bb0a1183549919fdab6022d10f6997826731
jpdenford <jpdenford@gmail.com>
2016-05-13 13:58:44 +1200

    PeerProgramming Added sequelize library

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,18 @@ var db = require('../lib/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db.connect(function(err, client, done) {
-    done(); // always need to call done after db calls
+  // db.connect(function(err, client, done) {
+  //   done(); // always need to call done after db calls
 
-    // db error checking
-    if (err) {
-      res.status(500).render('error', { 
-        message: "Something went wrong with the database",
-        error: err // should not dump error in production
-      });
-      return;
-    }
-
-    res.render('index', { title: 'Express' });
-  });
+  //   // db error checking
+  //   if (err) {
+  //     res.status(500).render('error', { 
+  //       message: "Something went wrong with the database",
+  //       error: err // should not dump error in production
+  //     });
+  //     return;
+  //   }
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
